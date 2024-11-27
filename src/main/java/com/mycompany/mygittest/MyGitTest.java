4e302c43dd5834a75534a1ef869c419e6e4d8e25
defranjo <joe.defranco@sheridancollege.ca>
2024-11-26 19:36:55 -0500

    My fourth comment

--- src/main/java/com/mycompany/mygittest/MyGitTest.java
+++ src/main/java/com/mycompany/mygittest/MyGitTest.java
@@ -10,6 +10,16 @@ package com.mycompany.mygittest;
  */
 public class MyGitTest {
 
+    private int myProperty;
+
+    public int getMyProperty() {
+        return myProperty;
+    }
+
+    public void setMyProperty(int myProperty) {
+        this.myProperty = myProperty;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
